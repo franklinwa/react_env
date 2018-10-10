11635b068602d48275345e6a769b80f1b7c660ee
franklinwa@163.com <franklinwa@163.com>
2018-10-10 14:38:54 +0800

    初次设置

---
+++ app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class App extends React.Component {
+
+    render() {
+        return <div>Hello World!React 编程开发环境建设好了！！！</div>
+    }
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+    publicPath:"/assets/"
+  },
+
+    devServer: {
+        // contentBase: './dist', //默认webpack-dev-server会为根文件夹提供本地服务器，如果想为另外一个目录下的文件提供本地服务器，应该在这里设置其所在目录（本例设置到"build"目录）
+       
+        inline: true,
+        port: 3333
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/, // babel 转换为兼容性的 js
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'latest']
+                }
+            }
+        ]
+    }
+}
