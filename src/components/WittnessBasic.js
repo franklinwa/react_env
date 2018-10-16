075cae7f2e8531f44c7ae739748c6aa5eddee0f2
franklinwa <franklinwa@163.com>
2018-10-16 13:48:06 +0800

    2018-10-16

--- src/components/WittnessBasic.js
+++ src/components/WittnessBasic.js
@@ -7,20 +7,21 @@ import  Heart  from './heart/Heart';
 import  EveryRate  from './heart/EveryRate.js';
 
 export const WittnessBasic = ({ name,date,pages,rating,onRate=f=>f}) =>
-    <section className="wittness-basic">
-       
+
+       <section className="wittness-basic">
        <div>
            
            <div className="content">
                <p>{date},阅读《{name}》第{pages[0].start}-{pages[0].end}页，特此见证，感谢坚持读！特此见证，感谢坚持读！</p>
            </div>
            
-            <div className="wittness">
+            <div className="wittness wittness-item">
            <Heart {...rating}  onClick = {() => (onRate())}/>
            <EveryRate {...rating}/>
            </div>
        </div>
+       </section>
     
 
-    </section>
+
     
--- src/components/heart/EveryRate.js
+++ src/components/heart/EveryRate.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 // 无状态函数模块
 const EveryRate = ({quantity}) =>(
-    <div>
+    <div className="item">
     <span>{quantity}
     </span>
     <span>{quantity}
